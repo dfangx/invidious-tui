fb51477fe0fa5595fb2e280d6d945e831fe5ae72
cyrusng <cyrus.ng@protonmail.com>
2020-02-29 01:47:11 -0800

    cleanup and simplified view system

--- src/app.rs
+++ src/app.rs
@@ -1,207 +1,93 @@
 use tokio::runtime::Runtime;
 use reqwest::Client;
-use failure::Error;
 use crate::{
     player::Player,
     config::Config,
+    data:: LoadedData,
+    ui::views::{
+        ViewType,
+        View,
+        ContentType,
+    },
+    invidious,
     utils,
-    search::{
-        Search,
-    }
 };
-use std::collections::HashMap;
-use std::sync::{
-    Arc,
-    RwLock,
+use std::{
+    collections::{
+        HashMap,
+        VecDeque,
+    },
+    sync::{
+        Arc,
+        RwLock,
+    },
 };
-
-#[derive(Clone, Debug)]
-pub enum ContentType {
-    MediaContent(Arc<RwLock<Vec<Vec<String>>>>),
-    ListContent(Vec<String>),
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-pub enum PaneType {
-    Settings,
-    Frontpage,
-    Search,
-    Videos,
-    Channels,
-    Playlists,
-    SearchResults,
-}
-
-#[derive(Clone, Debug)]
-pub struct Pane {
-    pub title: String,
-    pub selected: usize,
-    pub has_focus: bool,
-    pub content: ContentType,
-}
-
-impl Pane {
-    fn new(title: String, selected: usize, has_focus: bool, content: ContentType) -> Self {
-        Pane {
-            title,
-            selected,
-            has_focus,
-            content,
-        }
-    }
-}
-
-impl Default for Pane {
-    fn default() -> Self {
-        Pane {
-            title: String::new(),
-            selected: 0,
-            has_focus: false,
-            content: ContentType::ListContent(vec![]),
-        }
-    }
-}
-
-pub enum Mode {
-    Normal,
-    Selection,
-    Input,
-}
 
 pub struct App {
-    pub focused: PaneType,
-    pub mode: Mode,
-    pub panes: HashMap<PaneType, Pane>,
     pub quit: bool,
     pub config: Config,
     pub loaded_data: LoadedData,
-    pub main_pane: PaneType,
-    pub prev_focused: PaneType,
+   
+    pub cmdline_focused: bool,
     pub input: String,
+    
     pub client: Client,
-    pub runtime: Runtime,
+    pub runtime: Arc<RwLock<Runtime>>,
+    
     pub player: Player,
-    pub current_title: String,
-    pub current_author: String,
+    pub current_track: String,
+    pub next_track: String,
+    pub media_queue: VecDeque<(String, String)>,
+    
+    pub focused_view: ViewType,
+    pub view_list: HashMap<ViewType, View>,
 }
 
 impl App {
-    pub fn new(config: Config) -> Result<Self, Error> {
-        let main_pane = PaneType::Videos;
-        
-        Ok(App {
+    pub fn new(config: Config) -> Self {
+        App {
+            cmdline_focused: false,
             input: String::new(),
-            current_title: String::new(),
-            current_author: String::new(),
-            mode: Mode::Normal,
-            focused: PaneType::Settings,
-            panes: Self::init_pane_data(),
+            current_track: String::new(),
+            next_track: String::new(),
             loaded_data: LoadedData::default(),
             client: Client::new(),
-            runtime: Runtime::new().unwrap(),
+            runtime: Arc::new(RwLock::new(Runtime::new().unwrap())),
             player: Player::default(),
-            prev_focused: main_pane,
             quit: false,
-            main_pane,
+            media_queue: VecDeque::new(),
+            
+            focused_view: ViewType::Home,
+            view_list: HashMap::new(),
             config,
-        })
-    }
-
-
-    fn init_pane_data() -> HashMap<PaneType, Pane> {
-        let settings_list= vec!["Home".to_owned(), "Help".to_owned(), "Settings".to_owned(), "User Profile".to_owned()];
-        let frontpage_list = vec!["Trending".to_owned(), "Top Videos".to_owned(), "Popular".to_owned()];
-        let media_list= vec!["Videos".to_owned(), "Playlists".to_owned(), "Channels".to_owned()];
-        [
-            (PaneType::Settings, Pane::new("Settings".to_owned(), 
-                                           0, 
-                                           true, 
-                                           ContentType::ListContent(settings_list),
-                                           )),
-            (PaneType::Frontpage, Pane::new("Main Menu".to_owned(), 
-                                          0, 
-                                          false, 
-                                          ContentType::ListContent(frontpage_list),
-                                          )),
-            (PaneType::Search, Pane::new("Search".to_owned(), 
-                                           0, 
-                                           false, 
-                                           ContentType::ListContent(media_list),
-                                           )),
-            (PaneType::Videos, Pane::new("Videos".to_owned(), 
-                                         0, 
-                                         false, 
-                                         ContentType::MediaContent(Arc::new(RwLock::new(vec![]))),
-                                         )),
-            (PaneType::Channels, Pane::new("Channels".to_owned(), 
-                                           0, 
-                                           false, 
-                                           ContentType::MediaContent(Arc::new(RwLock::new(vec![]))),
-                                           )),
-            (PaneType::Playlists, Pane::new("Playlists".to_owned(), 
-                                            0, 
-                                            false, 
-                                            ContentType::MediaContent(Arc::new(RwLock::new(vec![]))),
-                                            )),
-        ].iter().cloned().collect::<HashMap<PaneType, Pane>>()
-    }
-
-    pub fn move_focus_down(&mut self) {
-        match self.focused {
-            PaneType::Settings => {
-                utils::swap_focus(self, PaneType::Frontpage);
-            },
-            PaneType::Frontpage => {
-                utils::swap_focus(self, PaneType::Search);
-            },
-            _ => {},
         }
     }
 
-    pub fn move_focus_right(&mut self) {
-        match self.focused {
-            PaneType::Videos 
-                | PaneType::Channels 
-                | PaneType::Playlists => {},
-            _ => utils::swap_focus(self, self.main_pane),
+    pub fn run_setup(&mut self) {
+        let client = &self.client;
+        let (trending, popular) = self.runtime.write().unwrap().block_on(invidious::load_home(&client)).unwrap();
+
+        let trending_text = utils::video_to_text(trending.clone());
+        let popular_text = utils::video_to_text(popular.clone());
+        //let top_text = search::video_to_text(top.clone());
+
+        if let Some(view) = self.view_list.get_mut(&ViewType::Home) {
+            if let Some(mut window) = view.root_windows.get_mut(0) {
+                window.content = ContentType::MediaContent(Arc::new(RwLock::new(trending_text)));
+                window.selected = 0;
+            }
+            if let Some(mut window) = view.root_windows.get_mut(1) {
+                window.content = ContentType::MediaContent(Arc::new(RwLock::new(popular_text)));
+                window.selected = 0;
+            }
         }
+        self.loaded_data.trending_videos = trending;
+        self.loaded_data.popular_videos = popular;
+        //self.loaded_data.top_videos = top;
     }
-
-    pub fn move_focus_left(&mut self) {
-        match self.focused {
-            PaneType::Videos 
-                | PaneType::Playlists 
-                | PaneType::Channels => utils::swap_focus(self, self.prev_focused),
-            _ => {}
-        }
+    pub fn view(mut self, view_type: ViewType, view: View) -> Self {
+        self.view_list.insert(view_type, view);
+        self
     }
-
-    pub fn move_focus_up(&mut self) {
-        match self.focused {
-            PaneType::Search => {
-                utils::swap_focus(self, PaneType::Frontpage);
-            },
-            PaneType::Frontpage => {
-                utils::swap_focus(self, PaneType::Settings);
-            },
-            _ => {},
-        }
-    }
-
-
 }
 
-use crate::media::Video;
-pub struct LoadedData {
-    pub search_data: Search,
-    pub videos: Vec<Video>,
-}
-
-impl Default for LoadedData {
-    fn default() -> Self {
-        LoadedData {
-            search_data: Search::default(),
-            videos: vec![],
-        }
-    }
-}
